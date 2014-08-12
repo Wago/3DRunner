52d6b747d6ff106b1b11e1bd14948efc095e7812
Wagosaur <fatkidscantfly@hotmail.com>
2014-08-12 23:37:20 +0200

    Inital Commit, only a few scripts

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			Instantiate(projectile,transform.position,transform.rotation);
+		}
+	}
+}

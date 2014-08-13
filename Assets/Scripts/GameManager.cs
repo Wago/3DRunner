7e00c740475a16faaedd13a608e584304946d962
Wagosaur <fatkidscantfly@hotmail.com>
2014-08-13 06:04:18 +0200

    Basic game manager and restart added.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(GameObject.FindGameObjectWithTag("Player") == null){
+
+			gameOver = true;
+
+		}
+		if(gameOver){
+
+			if(Input.anyKeyDown){
+
+				Application.LoadLevel(Application.loadedLevel);
+
+			}
+
+		}
+
+	}
+
+	void OnGUI(){
+
+		if(gameOver == true){
+			GUILayout.Label("Game over! Press any key to reset!");
+		}
+
+	}
+}

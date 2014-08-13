8d2087562a5348f5034eed8fc198050811188f95
Wagosaur <fatkidscantfly@hotmail.com>
2014-08-13 06:34:26 +0200

    Score system!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,34 +3,44 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	public static GameManager Instance;
+
+	public float pointsPerUnitTravelled = 1.0f;
+	public float gameSpeed = 10.0f;
+
+	private float score = 0.0f;
+	private static float highScore = 0.0f;
 	private bool gameOver = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		Instance = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 		if(GameObject.FindGameObjectWithTag("Player") == null){
-
 			gameOver = true;
-
 		}
 		if(gameOver){
-
 			if(Input.anyKeyDown){
-
 				Application.LoadLevel(Application.loadedLevel);
-
 			}
-
+		}
+		if(!gameOver){
+			score += pointsPerUnitTravelled*gameSpeed*Time.deltaTime;
+			if(score > highScore){
+				highScore = score;
+			}
 		}
 
 	}
 
 	void OnGUI(){
+		int currentScore = (int)score;
+		int currentHighScore = (int)highScore;
+		GUILayout.Label("Score: " + currentScore.ToString());
+		GUILayout.Label("High Score: " + currentHighScore.ToString());
 
 		if(gameOver == true){
 			GUILayout.Label("Game over! Press any key to reset!");

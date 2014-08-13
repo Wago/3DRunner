550cd2139afe9448aa463a768227a43e6a188f67
Wagosaur <fatkidscantfly@hotmail.com>
2014-08-13 08:11:39 +0200

    Messed up game manager with complexity, but at least title screen now displays previous score!

---
+++ Assets/Scripts/DisplayPreviousScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayPreviousScore : MonoBehaviour {
+
+	private string label;
+	
+	// Use this for initialization
+	void Start () {
+		//Get the text mesh on the current object and save it as textMesh
+		TextMesh textMesh = GetComponent<TextMesh>();
+		//Grabs the current text and assigns it to label.
+		label = textMesh.text;
+		//Loads the previous score from the game manger, and displays is after the lable!
+		textMesh.text = label + GameManager.Instance.previousScore;
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	//Referance it self for use in other scripts.
-	public static GameManager Instance;
+	//Referance it self for use else where, also appears when referanced if not already there.
+	public static GameManager Instance{
+		get{
+			//If _instance is not null return, else create a new game object
+			if(_instance != null){
+				return _instance;
+			}else{
+				GameObject gameManager = new GameObject("GameManager");
+				_instance = gameManager.AddComponent<GameManager>();
+				return _instance;
+			}
+		}
+	}
+
+	//Referance it self for use in THIS scripts.
+	private static GameManager _instance;
 
 	//Points per unit travelled and a unified game speed to move at
 	public float pointsPerUnitTravelled = 1.0f;
 	public float gameSpeed = 10.0f;
 	public string titleScreen = "Title";
+
+	[HideInInspector] //This hides it in the inspector, but it can still be seen by other scripts.
+	public int previousScore = 0;
 
 	//Just some variables
 	private float score = 0.0f;
@@ -19,39 +36,68 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//Make sure Instance is a referance to THIS game object!
-		Instance = this;
+		//Only do all of this if _instance is not this.
+		if(_instance != this){
+			//Only set this if its not already set.
+			if(_instance == null){
+				//Make sure Instance is a referance to THIS game object!
+				_instance = this;
+			//If instance is already set destory it.
+			}else{
+				Destroy(gameObject);
+			}
+		}
 		//Loads the highscore from the playerprefs.
 		LoadHighScore();
+		//Don't destory this game object on start, that way we can bring it in to the next scene.
+		DontDestroyOnLoad(gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Find game object with the tag player, and if that object don't exsist put game over to true.
-		if(GameObject.FindGameObjectWithTag("Player") == null){
-			gameOver = true;
-		}
-		//When game over is true, and key will restart the current loaded level
-		if(gameOver){
-			if(!hasSaved){
-				SaveHighScore();
-				hasSaved = true;
-			}if(Input.anyKeyDown){
-				Application.LoadLevel("Title");
+		//Only do this if you are not on the title screen!
+		if(Application.loadedLevelName != titleScreen){
+			//Find game object with the tag player, and if that object don't exsist put game over to true.
+			if(GameObject.FindGameObjectWithTag("Player") == null){
+				gameOver = true;
 			}
-		}
-		//When game over is true stop counting score
-		if(!gameOver){
-			score += pointsPerUnitTravelled*gameSpeed*Time.deltaTime;
-			if(score > highScore){
-				highScore = score;
+			//When game over is true, and key will restart the current loaded level
+			if(gameOver){
+				//If its not saved run this.
+				if(!hasSaved){
+					//Save highscore to playerpref.
+					SaveHighScore();
+					//Set previous score to current score.
+					previousScore = (int)score;
+					//Make sure this if statement is not run again.
+					hasSaved = true;
+				//On any keypress load the level named Title
+				}if(Input.anyKeyDown){
+					Application.LoadLevel("Title");
+				}
 			}
+			//When game over is true stop counting score
+			if(!gameOver){
+				score += pointsPerUnitTravelled*gameSpeed*Time.deltaTime;
+				if(score > highScore){
+					highScore = score;
+				}
+			}
+		}else{
+			//Resets things
+			ResetGame();
 		}
 		//Debug function to reset the highscore, when Y is pressed, then saves it to the playerpref.
 		if(Input.GetKeyDown(KeyCode.Y)){
 			highScore = 0;
 			SaveHighScore();
 		}
+	}
+	//Sets up for a reset;
+	void ResetGame(){
+		score = 0.0f;
+		gameOver = false;
+		hasSaved = false;
 	}
 	//Pusheds the highscore to the playerpref(A file saved locally on all platforms.
 	//Then saves it!
@@ -65,18 +111,20 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		//Convert current score to an in and display it on the screen
-		int currentScore = (int)score;
-		GUILayout.Label("Score: " + currentScore.ToString());
+		//Only display the gui text when not on the title screen.
+		if(Application.loadedLevelName != titleScreen){
+			//Convert current score to an in and display it on the screen
+			int currentScore = (int)score;
+			GUILayout.Label("Score: " + currentScore.ToString());
 
-		//Convert current HIGH score to an in and display it on the screen
-		int currentHighScore = (int)highScore;
-		GUILayout.Label("High Score: " + currentHighScore.ToString());
+			//Convert current HIGH score to an in and display it on the screen
+			int currentHighScore = (int)highScore;
+			GUILayout.Label("High Score: " + currentHighScore.ToString());
 
-		//When game over becomes true (you die) display text!
-		if(gameOver == true){
-			GUILayout.Label("Game over! Press any key to quit!");
+			//When game over becomes true (you die) display text!
+			if(gameOver == true){
+				GUILayout.Label("Game over! Press any key to quit!");
+			}
 		}
-
 	}
 }

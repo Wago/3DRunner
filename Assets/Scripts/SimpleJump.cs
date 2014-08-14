7cd30132d0e0996701311c59700f63ba0dbe97e9
Wagosaur <fatkidscantfly@hotmail.com>
2014-08-14 20:59:26 +0200

    Added jump and walls, removed ramps from the game.

---
+++ Assets/Scripts/SimpleJump.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleJump : MonoBehaviour {
+
+	public float jumpForce = 1.0f;
+	public bool jumped = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump") && transform.position.y <= 1.0f){
+			rigidbody.AddForce(0,jumpForce,0);
+		}
+	}
+}

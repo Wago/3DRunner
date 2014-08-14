f42370d33d547a5193940ebcffb8eaab22a4e670
Wagosaur <fatkidscantfly@hotmail.com>
2014-08-14 06:17:59 +0200

    Started on adding touchcontrols.

---
+++ Assets/Scripts/DetectTouchesAndClicks.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectTouchesAndClicks : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit)){
+				Debug.Log ("Object name: " + hit.collider.name, hit.collider.gameObject);
+			}
+		}
+	}
+}

f7baf3e7d2de8b752a8ebb36e851014acb89d465
Wagosaur <fatkidscantfly@hotmail.com>
2014-08-14 09:59:10 +0200

    added UI things, prepping for highscores

---
+++ Assets/Scripts/LoadLevelOnTouched.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelOnTouched : MonoBehaviour {
+
+	public string levelName;
+	
+	void Touched(){
+		Application.LoadLevel(levelName);
+	}
+}

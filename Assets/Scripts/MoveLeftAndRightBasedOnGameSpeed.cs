a451f154560e8284a0411a06b3afb62b2201f568
Wagosaur <fatkidscantfly@hotmail.com>
2014-08-19 10:48:18 +0200

    Added CC0 models made by Eracoon on www.opengameart.com Replaced the groud with a road, player with a car, and incoming obstacles with a gokart.

--- Assets/Scripts/MoveLeftAndRightBasedOnGameSpeed.cs
+++ Assets/Scripts/MoveLeftAndRightBasedOnGameSpeed.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class MoveLeftAndRightBasedOnGameSpeed : MonoBehaviour {
 		
+	public bool invertDirection = false;
+	public bool lockMovement = true;
+	public float LockOnX = 9.5f;
 	public bool dashEnable = false;
 	public float dashSpeed = 2.0f;
 	public float dashDuration = 1.0f;
@@ -25,8 +28,22 @@ public class MoveLeftAndRightBasedOnGameSpeed : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		speed = -GameManager.Instance.gameSpeed;
+		if(invertDirection){
+			speed = -GameManager.Instance.gameSpeed;
+		}else{
+		speed = GameManager.Instance.gameSpeed;
+		}
+
 		transform.position += Vector3.right*Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+
+		//Prevent movement in X
+		if(lockMovement){
+			if(transform.position.x <= -LockOnX){
+				transform.position = new Vector3(-LockOnX,transform.position.y,transform.position.z);
+			}else if(transform.position.x >= LockOnX){
+				transform.position = new Vector3(LockOnX,transform.position.y,transform.position.z);
+			}
+		}
 
 		if(dashEnable == true){
 			speed = -GameManager.Instance.gameSpeed;
